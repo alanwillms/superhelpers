9b48eed6bc321779d0716aa825198bcb65078d7c
Alan Willms <dyulax@gmail.com>
2015-11-28 16:03:01 -0200

    Add most of string handler methods

--- src/StringHandler.php
+++ src/StringHandler.php
@@ -9,22 +9,41 @@ class StringHandler
         return mb_strtoupper(mb_substr($self, 0, 1)) . mb_substr($self, 1);
     }
 
-    public function chars(string $self) : array
-    {
-        if ($self === '') {
-            return [];
-        }
-        return str_split($self);
-    }
-
     public static function downcase(string $self) : string
     {
         return mb_strtolower($self);
     }
 
+    public static function upcase(string $self) : string
+    {
+        return mb_strtoupper($self);
+    }
+
+    public function chars(string $self) : array
+    {
+        if ($self === '') {
+            return [];
+        }
+        return preg_split('//u', $self, -1, PREG_SPLIT_NO_EMPTY);
+    }
+
     public static function isEmpty(string $self) : bool
     {
         return $self === '';
+    }
+
+    public static function startsWith(string $self, ...$others) : bool
+    {
+        if (count($others) == 1) {
+            $others = (array) $others[0];
+        }
+
+        foreach ($others as $other) {
+            if ($other === '' || 0 === mb_strpos($self, $other)) {
+                return true;
+            }
+        }
+        return false;
     }
 
     public static function endsWith(string $self, ...$others) : bool
@@ -51,13 +70,113 @@ class StringHandler
         return str_replace($from, $to, $self);
     }
 
-    public static function regexReplace(string $self, string $pattern, string $replacement, string $modifiers = 'msr') : string
+    public static function regexReplace(string $self, string $pattern, string $replacement) : string
     {
-        return mb_ereg_replace($pattern, $replacement, $self, $modifiers);
+        return preg_replace($pattern, $replacement, $self);
     }
 
     public static function includes(string $self, string $other) : bool
     {
         return false !== mb_strpos($self, $other);
+    }
+
+    public static function index(string $self, string $substring, int $offset = 0)
+    {
+        return mb_strpos($self, $substring, $offset);
+    }
+
+    public static function length(string $self) : int
+    {
+        return mb_strlen($self);
+    }
+
+    public function lines(string $self) : array
+    {
+        if ($self === '') {
+            return [];
+        }
+        return explode("\n", $self);
+    }
+
+    public static function center(string $self, int $length, string $padding = ' ') : string
+    {
+        return self::pad($self, $length, $padding, STR_PAD_BOTH);
+    }
+
+    public static function padLeft(string $self, int $length, string $padding = ' ') : string
+    {
+        return self::pad($self, $length, $padding, STR_PAD_LEFT);
+    }
+
+    public static function padRight(string $self, int $length, string $padding = ' ') : string
+    {
+        return self::pad($self, $length, $padding, STR_PAD_RIGHT);
+    }
+
+    public static function trim(string $self) : string
+    {
+        return trim($self);
+    }
+
+    public static function trimLeft(string $self) : string
+    {
+        return ltrim($self);
+    }
+
+    public static function trimRight(string $self) : string
+    {
+        return rtrim($self);
+    }
+
+    public static function matches(string $self, string $regex) : bool
+    {
+        return 1 === preg_match($regex, $self);
+    }
+
+    public static function reverse(string $self) : string
+    {
+        return implode(array_reverse($self->chars()));
+    }
+
+    public static function split(string $self, string $separator = ' ') : array
+    {
+        return array_values(array_filter(explode($separator, $self)));
+    }
+
+    public static function toBool(string $self) : bool
+    {
+        return boolval($self);
+    }
+
+    public static function toFloat(string $self) : float
+    {
+        return floatval($self);
+    }
+
+    public static function toInt(string $self, int $base = 10) : int
+    {
+        return intval($self, $base);
+    }
+
+    public static function toArray(string $self) : array
+    {
+        return $self->chars();
+    }
+
+    /**
+     * @see http://stackoverflow.com/questions/14773072/php-str-pad-unicode-issue
+     */
+    private static function pad($string, $length, $padding = ' ', $direction = STR_PAD_RIGHT)
+    {
+        $padBefore = $direction === STR_PAD_BOTH || $direction === STR_PAD_LEFT;
+        $padAfter = $direction === STR_PAD_BOTH || $direction === STR_PAD_RIGHT;
+        $length -= mb_strlen($string);
+        $targetLen = $padBefore && $padAfter ? $length / 2 : $length;
+        $stringToRepeatLength = mb_strlen($padding);
+        $repeatTimes = ceil($targetLen / $stringToRepeatLength);
+        $repeatedString = str_repeat($padding, max(0, $repeatTimes));
+        $before = $padBefore ? mb_substr($repeatedString, 0, floor($targetLen)) : '';
+        $after = $padAfter ? mb_substr($repeatedString, 0, ceil($targetLen)) : '';
+        return $before . $string . $after;
     }
 }
--- tests/StringHandlerTest.php
+++ tests/StringHandlerTest.php
@@ -13,17 +13,23 @@ class StringHandlerTest extends \PHPUnit_Framework_TestCase
         $this->assertEquals('Âncora', 'âncora'->capitalize());
     }
 
+    public function testDowncase()
+    {
+        $this->assertEquals('hello', 'hEllO'->downcase());
+        $this->assertEquals('açaí', 'AÇAÍ'->downcase());
+    }
+
+    public function testUpcase()
+    {
+        $this->assertEquals('HELLO', 'hEllO'->upcase());
+        $this->assertEquals('AÇAÍ', 'açaí'->upcase());
+    }
+
     public function testChars()
     {
         $this->assertEquals(['h', 'e', 'l', 'l', 'o'], 'hello'->chars());
         $this->assertEquals(['h', 'i', ' ', 'm', 'a', 'n'], 'hi man'->chars());
         $this->assertEquals([], ''->chars());
-    }
-
-    public function testDowncase()
-    {
-        $this->assertEquals('hello', 'hEllO'->downcase());
-        $this->assertEquals('açaí', 'AÇAÍ'->downcase());
     }
 
     public function testIsEmpty()
@@ -53,7 +59,7 @@ class StringHandlerTest extends \PHPUnit_Framework_TestCase
 
     public function testRegexReplace()
     {
-        $this->assertEquals('cute cat, cute cats', '1 cat, 2 cats'->regexReplace('\d{1,}', 'cute'));
+        $this->assertEquals('cute cat, cute cats', '1 cat, 2 cats'->regexReplace('/\d{1,}/', 'cute'));
     }
 
     public function testIncludes()
@@ -62,8 +68,146 @@ class StringHandlerTest extends \PHPUnit_Framework_TestCase
         $this->assertFalse('hello'->includes('ol'));
     }
 
-    // @ignored
-    // center
+    public function testIndex()
+    {
+        $this->assertEquals(1, 'hello'->index('e'));
+        $this->assertEquals(3, 'hello'->index('lo'));
+        $this->assertEquals(2, 'hello'->index('l'));
+        $this->assertEquals(3, 'hello'->index('l', 3));
+        $this->assertFalse('hello'->index('a'));
+    }
+
+    public function testLength()
+    {
+        $this->assertEquals(5, 'hello'->length());
+        $this->assertEquals(0, ''->length());
+        $this->assertEquals(4, 'açaí'->length());
+    }
+
+    public function testLines()
+    {
+        $this->assertEquals(['hello', 'world'], "hello\nworld"->lines());
+        $this->assertEquals([], ''->lines());
+    }
+
+    public function testCenter()
+    {
+        $this->assertEquals('hello', 'hello'->center(4));
+        $this->assertEquals('       hello        ', 'hello'->center(20));
+        $this->assertEquals('1231231hello12312312', 'hello'->center(20, '123'));
+        $this->assertEquals('=açaí=', 'açaí'->center(6, '='));
+    }
+
+    public function testPadLeft()
+    {
+        $this->assertEquals('hello', 'hello'->padLeft(4));
+        $this->assertEquals('               hello', 'hello'->padLeft(20));
+        $this->assertEquals('123412341234123hello', 'hello'->padLeft(20, '1234'));
+        $this->assertEquals('==açaí', 'açaí'->padLeft(6, '='));
+    }
+
+    public function testPadRight()
+    {
+        $this->assertEquals('hello', 'hello'->padRight(4));
+        $this->assertEquals('hello               ', 'hello'->padRight(20));
+        $this->assertEquals('hello123412341234123', 'hello'->padRight(20, '1234'));
+        $this->assertEquals('açaí==', 'açaí'->padRight(6, '='));
+    }
+
+    public function testTrim()
+    {
+        $this->assertEquals('hello', '  hello  '->trim());
+        $this->assertEquals('hello', '  hello'->trim());
+        $this->assertEquals('hello', 'hello  '->trim());
+        $this->assertEquals('hello', 'hello'->trim());
+    }
+
+    public function testTrimLeft()
+    {
+        $this->assertEquals('hello  ', '  hello  '->trimLeft());
+        $this->assertEquals('hello', '  hello'->trimLeft());
+        $this->assertEquals('hello', 'hello'->trimLeft());
+    }
+
+    public function testTrimRight()
+    {
+        $this->assertEquals('  hello', '  hello  '->trimRight());
+        $this->assertEquals('hello', 'hello  '->trimRight());
+        $this->assertEquals('hello', 'hello'->trimRight());
+    }
+
+    public function testMatches()
+    {
+        $this->assertTrue('100 cats'->matches('/\d/'));
+        $this->assertTrue('100 cats'->matches('/[a-z]/'));
+        $this->assertFalse('100 cats'->matches('/dog/'));
+    }
+
+    public function testReverse()
+    {
+        $this->assertEquals('desserts', 'stressed'->reverse());
+        $this->assertEquals('íaça', 'açaí'->reverse());
+    }
+
+    public function testSplit()
+    {
+        $this->assertEquals(['now\'s', 'the', 'time'], " now's  the time"->split());
+        $this->assertEquals(['now\'s', 'the', 'time'], " now's  the time"->split(' '));
+        $this->assertEquals(['1', '2', '3'], '1,2,3'->split(','));
+        $this->assertEquals(['2', '3'], ',2,3'->split(','));
+        $this->assertEquals([], ','->split(','));
+    }
+
+    public function testStartsWith()
+    {
+        $this->assertTrue('hello'->startsWith(''));
+        $this->assertTrue('hello'->startsWith('hell'));
+        $this->assertTrue('hello'->startsWith('heaven', 'hell'));
+        $this->assertTrue('hello'->startsWith(['heaven', 'hell']));
+        $this->assertFalse('hello'->startsWith('paradise'));
+        $this->assertFalse('hello'->startsWith('heaven', 'paradise'));
+        $this->assertFalse('hello'->startsWith(['heaven', 'paradise']));
+    }
+
+    public function testToBool()
+    {
+        $this->assertTrue('string'->toBool());
+        $this->assertTrue('true'->toBool());
+        $this->assertTrue('false'->toBool());
+        $this->assertTrue('1'->toBool());
+        $this->assertFalse(''->toBool());
+        $this->assertFalse('0'->toBool());
+    }
+
+    public function testToFloat()
+    {
+        $this->assertEquals(42.0, '42'->toFloat());
+        $this->assertEquals(93.56, '93.56'->toFloat());
+        $this->assertEquals(1234.5, '123.45e1'->toFloat());
+        $this->assertEquals(45.67, '45.67 degrees'->toFloat());
+        $this->assertEquals(0.0, 'thx1138'->toFloat());
+    }
+
+    public function testToInt()
+    {
+        $this->assertEquals(12345, '12345'->toInt());
+        $this->assertEquals(99, '99 red balloons'->toInt());
+        $this->assertEquals(0, '0a'->toInt());
+        $this->assertEquals(10, '0a'->toInt(16));
+        $this->assertEquals(0, 'hello'->toInt());
+        $this->assertEquals(101, '1100101'->toInt(2));
+        $this->assertEquals(294977, '1100101'->toInt(8));
+        $this->assertEquals(1100101, '1100101'->toInt(10));
+        $this->assertEquals(17826049, '1100101'->toInt(16));
+    }
+
+    public function testToArray()
+    {
+        $this->assertEquals(['h', 'e', 'l', 'l', 'o'], 'hello'->toArray());
+        $this->assertEquals([], ''->toArray());
+    }
+
+    // Main ignored methods from Ruby String API:
     // chomp
     // count
     // crypt
@@ -71,4 +215,10 @@ class StringHandlerTest extends \PHPUnit_Framework_TestCase
     // dump
     // encode
     // hash
+    // insert
+    // partition
+    // prepend
+    // squeeze
+    // swapcase
+    // tr
 }
